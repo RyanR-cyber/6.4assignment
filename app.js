c1309c3b0f8aec5348b48075aafbc39b42051cfd
RyanR-cyber <ozzy19992@gmail.com>
2025-10-06 16:53:56 -0500

    intlize

---
+++ app.js
@@ -0,0 +1,28 @@
+const car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2020,
+    color: 'Blue'
+}; // Object literal
+
+console.log(car);
+
+const games = {
+    titleName: 'The Legend of Zelda',
+    genreName: 'Adventure',
+    platformName: 'Nintendo Switch',
+    fullName: function() {
+        return `${this.titleName} + 's on ' + ${this.platformName}`;
+    }   
+    
+}    ; // Object literal with method
+
+console.log(games.fullName());
+
+const marvel = {"spiderman", "ironman", "hulk", "thor"}; // Object literal with array (incorrect syntax)
+console.log(marvel);
+
+const dc = {"batman", "superman", "wonderwoman", "flash"}; 
+
+let size = dc.length;
+console.log(size);
